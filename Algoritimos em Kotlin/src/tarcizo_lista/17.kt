ff5992d945fe3ad827150b0dfddd78fbbcdd7c9d
Tarcizo Neto <tarcizon12@gmail.com>
2022-03-04 16:16:41 -0300

    Update 17

--- Algoritimos em Kotlin/src/tarcizo_lista/17.kt
+++ Algoritimos em Kotlin/src/tarcizo_lista/17.kt
@@ -1,4 +1,5 @@
 package tarcizo_list
+
 private fun promptInt(message: String): Int {
     print(message)
     return readLine()!!.toInt()
@@ -9,10 +10,55 @@ private fun promptString(message: String): String {
     return readLine()!!.toString()
 }
 
-fun main() {
-        var product = arrayOf(String)
-        var price = IntArray(5)
-        product[1]= "Banana" as String.Companion
+fun howManyProducs(array: MutableList<Int>): Int{
+    var theAmount = 0
 
-    print(product[1])
+    repeat(array.filter{it < 50}.size) {
+        theAmount++
+    }
+
+    return theAmount
+}
+
+fun productsInRange(arrayPrice: MutableList<Int>, arrayProduct: MutableList<String>): String {
+    val products = mutableListOf<String>()
+
+    arrayPrice.filter{it in 50..100}.forEach{
+
+        products.add(arrayProduct[arrayPrice.indexOf(it)])
+
+    }
+
+    return products.toString()
+}
+
+fun overHundred(arrayPrice: MutableList<Int>): Double{
+    val pricesOverHundre = mutableListOf<Int>()
+
+    arrayPrice.forEach{
+        if(it>100){
+            pricesOverHundre.add(it)
+        }
+    }
+
+    val average = pricesOverHundre.average()
+    return average
+}
+
+fun main() {
+        val products = mutableListOf<String>()
+        val prices = mutableListOf<Int>()
+
+        for (i in 0 until 3){
+            products.add(promptString("Digite o nome do produto ${i+1} "))
+            prices.add(promptInt("Digite o valor desse produto ${i+1} "))
+
+        }
+
+    println("A quantidade de produtos com preços inferior a R$50,00 ${howManyProducs(prices)}: ")
+    println("O nome dos produtos com preço entre R$ 50,00 e R$ 100,00 são: ${productsInRange(prices,products)}")
+    println("a média dos preços dos produtos com preço superior a R$ 100,00 é de: ${overHundred(prices)}")
+
+
+
 }
