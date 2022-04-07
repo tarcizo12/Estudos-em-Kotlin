7de4edfff2ad9148f5c8757a9d030a296ef21a3e
Tarcizo Neto <tarcizon12@gmail.com>
2022-04-07 16:34:41 -0300

    Abrindo maps

--- ProjetosDeAndroid/IntentsComuns/app/src/main/java/com/example/intentscomuns/MainActivity.kt
+++ ProjetosDeAndroid/IntentsComuns/app/src/main/java/com/example/intentscomuns/MainActivity.kt
@@ -8,21 +8,40 @@ import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    val urlWhatsapp = "https://api.whatsapp.com/send?phone=+5585986564561"
+    val urlMaps = "https://goo.gl/maps/uzixnx67aoAg9rhq9"
     lateinit var whatsapp: Button
-    val url = "https://api.whatsapp.com/send?phone=+5585986564561"
+    lateinit var googleMaps: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         whatsapp = findViewById(R.id.buttonWhatsApp)
         whatsapp.setOnClickListener(this)
+
+        googleMaps = findViewById(R.id.buttonMaps)
+        googleMaps.setOnClickListener(this)
     }
 
-    override fun onClick(p0: View?) {
+    override fun onClick(buttonPressed: View?) {
+
+        when(buttonPressed?.id){
+            R.id.buttonWhatsApp -> openWhatsapp()
+            R.id.buttonMaps -> openMaps()
+        }
+
+    }
+
+    private fun openWhatsapp() {
         val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(url)
+        intent.data = Uri.parse(urlWhatsapp)
         startActivity(intent)
-
     }
-    
+
+    private fun openMaps() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(urlMaps)
+        startActivity(intent)
+    }
+
 }
